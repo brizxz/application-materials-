46e58ad1fe04c7527dfd294aef6a3de85749e096
brizxz <98811972+brizxz@users.noreply.github.com>
2022-04-30 19:10:29 +0800

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,14 @@
+from ensurepip import version
+import qrcode
+import image
+qr=qrcode.QRCode(
+    version = 15,
+    box_size = 10,
+    border=6
+)
+data="https://youtu.be/kYhh1PpsOg4"
+
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black", back_color="white")
+img.save("test.png")
